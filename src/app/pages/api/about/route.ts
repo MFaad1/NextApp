533bd2ad1eb6baa91c60effed964834916a6ae78
faad-50 <faad.gtp@pk.see.biz>
2023-06-13 17:45:27 +0500

    adding new changes

--- src/app/pages/api/about/route.ts
+++ src/app/pages/api/about/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse} from "next/server";
 
-import axios  from "axios";
+import axios from 'axios'
 
 export const POST= async(req: Request,)=>{
     const values = await req.json()
-    console.log(values, 'front Input')
    let {data} =await axios.post('http://localhost:3200/data', values)
 
    return NextResponse.json({mesage: "Data has been found", data});
