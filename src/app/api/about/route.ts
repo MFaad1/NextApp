6e6c2603446cec46266c9b26f05b2e689ca6ec58
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 14:37:07 +0500

    add headers in the request

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -5,7 +5,13 @@ import axios from 'axios'
 export const POST= async(req: Request,)=>{
 
     const values = await req.json()
-   let {data} = await axios.post('https://node-ts-be.vercel.app/data', values)
+   let {data} = await axios.post('https://node-ts-be.vercel.app/data', values,{
+    headers: {
+      'Access-Control-Allow-Origin': 'https://next-app-trail-proj.vercel.app',
+      'Access-Control-Allow-Methods': 'POST',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  })
    return NextResponse.json({mesage: "Data has been found", data});
 }
 
