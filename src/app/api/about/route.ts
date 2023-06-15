98d2c280174de4166f37cda1f638fca73ca4e1d1
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 14:04:09 +0500

    adding changes for cors

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse} from "next/server";
-
 import axios from 'axios'
 
 export const POST= async(req: Request,)=>{
+
     const values = await req.json()
    let {data} = await axios.post('https://node-ts-be.vercel.app/data', values)
    return NextResponse.json({mesage: "Data has been found", data});
