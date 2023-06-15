61116e3776b0ec9e33a545a3c17025928716dbd8
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 15:30:38 +0500

    add new values

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "https://next-app-trail-proj.vercel.app*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
