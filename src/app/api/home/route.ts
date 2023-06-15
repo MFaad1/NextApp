59302a4bea37441d5ca60a003d673b1fc9975477
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 14:15:58 +0500

    latest

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -3,7 +3,7 @@ import { NextResponse} from "next/server";
 
 export const GET = async () => {
   try {
-    let data = await axios.get("http://localhost:3200/Api");
+    let data = await axios.get("'https://node-ts-be.vercel.app/data");
     return new NextResponse(data.data,{status: 200})
   } catch (err:any) {
     return new NextResponse(err,{status: 200})
