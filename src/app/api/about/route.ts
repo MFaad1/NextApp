2e8ebe689c4463050401f4bddf14ba1b764d879a
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 13:19:56 +0500

    Changing the url

--- src/app/api/about/route.ts
+++ src/app/api/about/route.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export const POST= async(req: Request,)=>{
     const values = await req.json()
-   let {data} = await axios.post('http://localhost:3200/data', values)
+   let {data} = await axios.post('https://node-ts-be.vercel.app/data', values)
    return NextResponse.json({mesage: "Data has been found", data});
 }
 
