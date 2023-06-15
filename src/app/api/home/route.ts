7a5460b9d4a9b937024995d4ae3a77f4e2c0aa99
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 16:35:36 +0500

    adding new changes

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -3,6 +3,14 @@ import { NextResponse} from "next/server";
 
 export const GET = async () => {
   try {
+    return new Response('Hello, Next.js!', {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      }
+    });
     // let data = await axios.get("'https://node-ts-be.vercel.app/data");
     return new NextResponse("Hellow this is response",{status: 200})
   } catch (err:any) {
