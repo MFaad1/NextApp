c40fd35abff9dbef9584e30e2c93d56f2b0c3e4b
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 14:30:48 +0500

    add changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,12 +4,13 @@ import axios from 'axios'
 function About() {
 
   const handleClick = () => {
+    console.log('handleclick')
     axios.get('https://next-app-trail-proj-qnu7gfmhu-mfaad1.vercel.app/api/home')
     .then((res:any)=>{
         alert(res.data)
         console.log(res.data)
     })
-    .catch((err:any)=>{console.log("Error has been resulved", err.messag )})
+    .catch((err:any)=>{console.log("Error has been resulved", err )})
   }; 
   return (
     <>
